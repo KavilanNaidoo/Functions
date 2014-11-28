810f6be365e91d94abf4cd6d980b34c7b4b50371
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-11-28 14:31:04 +0000

    Conversions

---
+++ conversion functions(unf).py
@@ -0,0 +1,21 @@
+#Kavilan Naidoo
+#28-11-2014
+#conversion rate
+
+def conversion():
+
+
+def input_conversion():
+    to_conversion = input("Please enter either US dollars, euros or pounds you want to convert to: ")
+    from_conversion = input("Please enter either US dollars, euros or pounds you want to convert from: ")
+    amount = float(input("Please enter the amount you want converted: "))
+return to_conversion, from_conversion, amount
+
+
+def GBP_Euro(amount_converted):
+    converted = amount * 1.229
+    print("{0.2f)".format(converted))
+
+    
+        
+
